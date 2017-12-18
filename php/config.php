d7995cbabb5c529696cbf89de74d64155d22c411
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-12-18 16:57:28 +0300

    changes

---
+++ php/config.php
@@ -0,0 +1,7 @@
+<?php
+//This is the config.php file
+
+// Specify your login credentials
+$username   = "Alex Kinuthia";
+$apikey     = "6463b91bd2487a21d544aba9f60f8da4b48625d5a2d9fcbb78f4fc6220005bb9";
+?>
